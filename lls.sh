71969d50873ecee206e2b7e20311c7e7e82a252a
Colin McDonald <colinmcd0731@gmail.com>
2021-10-11 15:31:44 -0400

    Get colors to work for lls.sh

--- lls.sh
+++ lls.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-ls -loghA --group-directories-first --time-style="+%Y-%m-%d %H:%M:%S"
+lls_do_color=
+if [ -t 1 ]
+then lls_do_color="--color=always"
+fi
+
+ls -loghAF --group-directories-first --time-style="+%Y-%m-%d %H:%M:%S" $lls_do_color $@ | perl -pe 's/(\S*)(\s+)(\S*)\s(.*)/\4/g'
+
