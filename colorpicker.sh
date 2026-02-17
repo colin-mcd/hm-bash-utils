4dde012d5a863ab63fd3a4588030c16bef3f9194
Colin McDonald <colinmcd0731@gmail.com>
2026-02-17 13:21:13 -0500

    Add color picker command

--- colorpicker.sh
+++ colorpicker.sh
@@ -23,9 +23,6 @@ if [ $# -ge 1 ]; then
     esac
 fi
 
-echo $COLOR_FORMAT
-echo $FP_PRECISION
-
 # from https://unix.stackexchange.com/a/724963
 colormsg=$(gdbus call --session --dest org.gnome.Shell.Screenshot --object-path /org/gnome/Shell/Screenshot --method org.gnome.Shell.Screenshot.PickColor)
 ##drop first 13 and last 5 characters: ({'color': <(...)>},)
@@ -38,13 +35,13 @@ SPACING=" "
 for ((i = 0; i < ${#colors[@]}; i++)); do
     case "$COLOR_FORMAT" in
         rgb) colors[$i]=$(printf '%.0f' $(echo "${colors[$i]} * 255" | bc));;
-        hex) colors[$i]=$(printf '%02x' $(printf '%.0f' $(echo "${colors[$i]} * 255" | bc)));
+        hex) colors[$i]=$(printf '%02X' $(printf '%.0f' $(echo "${colors[$i]} * 255" | bc)));
              SPACING="";; # no spaces between R, G, B fields
         fp)  colors[$i]=$(printf "%.0${FP_PRECISION}f" "${colors[$i]}");;
     esac
 done
 
-echo "${colors[0]}${SPACING}${colors[1]}${SPACING}${colors[2]}"
+echo -n "${colors[0]}${SPACING}${colors[1]}${SPACING}${colors[2]}" | wl-copy
 # echo   "RGB: ${colors[0]} ${colors[1]} ${colors[2]}"
 # printf "HEX: %02x%02x%02x\n" "${colors[0]}" "${colors[1]}" "${colors[2]}"
 #echo "$colors"
---
+++ install/xclip.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo apt install wl-clipboard
+sudo apt install xclip
